6205afec2aa8f10e7fbda2f6cca99f2893dd7ae7
Denis Laliberté <denis@tp1.ca>
2015-02-09 12:33:20 -0500

    add canary test

---
+++ search_spec.rb
@@ -0,0 +1,9 @@
+
+describe 'search' do
+  it "pass the canary test" do
+    expect(true).to eq(true)
+  end
+ # it " return a not found flag when search an int in empty list" do
+ #   expect
+ # end
+end
