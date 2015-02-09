dc111cd38f0676515feb1aaa287dcbdbd17288b1
Denis Laliberté <denis@tp1.ca>
2015-02-09 15:06:00 -0500

    refactor test

--- search_spec.rb
+++ search_spec.rb
@@ -8,12 +8,12 @@ describe 'search' do
   it " return a not found flag when search an int in empty list" do
     expect(search([],1)).to eq(-1)
   end
-  it "return the first index on one item list" do
+  it "return the first index" do
     expect(search([1],1)).to eq(0)
     expect(search([1],2)).to eq(-1)
+    expect(search([1,2,3],1)).to eq(0)
   end
   it "find the middle item" do
     expect(search([1,2,3],2)).to eq(1)
-    expect(search([1,2,3],1)).to eq(0)
   end
 end
