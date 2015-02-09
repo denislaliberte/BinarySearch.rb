bed453589bf0b594feef2f02540d7df02da57db9
Denis Laliberté <denis@tp1.ca>
2015-02-09 16:47:15 -0500

    refactor

--- search.rb
+++ search.rb
@@ -6,9 +6,11 @@ def search(array,item)
   middleIndex = array.length / 2
   if array[middleIndex] == item
     return middleIndex
-  elsif item < array[middleIndex] and middleIndex != 0
+  elsif middleIndex == 0
+    return -1
+  elsif item < array[middleIndex]
     return search(array[0..middleIndex-1],item)
-  elsif item > array[middleIndex] and middleIndex != 0
+  elsif item > array[middleIndex]
     asdf = search(array[middleIndex..-1],item)
     if(asdf != -1)
       return middleIndex + asdf
